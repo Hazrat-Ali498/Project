5094f84f8a8523b0d9a9422eb710b77ef2f82acb
Hazrat-Ali498 <me.ali5666@gmail.com>
2023-12-20 21:43:22 +0500

    Main

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter} from "react-router-dom";
 import FrontContext from "./Components/Context";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
  <FrontContext>
    <BrowserRouter>
-    <App />
+    <GoogleOAuthProvider clientId="<244972469922-124sva6dm2k5flivrjmagnq69qg91ukb.apps.googleusercontent.com>">
+      <App />
+    </GoogleOAuthProvider>
   </BrowserRouter>
  </FrontContext>
 );
